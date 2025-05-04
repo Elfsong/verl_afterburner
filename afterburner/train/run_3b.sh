72fe16afce9c25a5f9a7982cf8986604f4cc84ca
Elfsong <dumingzhe@126.com>
2025-05-04 12:29:58 +0000

    Update Parameters

--- afterburner/train/run_3b.sh
+++ afterburner/train/run_3b.sh
@@ -35,8 +35,8 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=16 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
     actor_rollout_ref.rollout.name=vllm \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.7 \
-    actor_rollout_ref.rollout.n=16 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.5 \
+    actor_rollout_ref.rollout.n=32 \
     actor_rollout_ref.rollout.temperature=1.0 \
     actor_rollout_ref.rollout.max_num_batched_tokens=163840 \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=16 \
@@ -47,7 +47,7 @@ python3 -m verl.trainer.main_ppo \
     trainer.project_name='verl_grpo_afterburner' \
     trainer.experiment_name='qwen2_3b_venus' \
     trainer.val_before_train=False \
-    trainer.n_gpus_per_node=4 \
+    trainer.n_gpus_per_node=8 \
     trainer.nnodes=1 \
     trainer.save_freq=20 \
     trainer.test_freq=20 \
